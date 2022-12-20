1e15788d595d5a4373010c4f13c79b4f6d1cc394
Alexandra <matildakrokodilda@gmail.com>
2022-12-20 09:03:16 +0300

    удалила закомментированный код

--- 1.js
+++ 1.js
@@ -3,21 +3,3 @@ let farengeiht = parseFloat(9 / 5 * celsius + 32);
 alert(`Цельсий: ${celsius} Фаренгейт: ${farengeiht.toFixed(1)}`)
 
 
-// const a = Number.parseInt(prompt('Введите число a:'));
-// const b = +prompt('Введите число b:');
-
-// function sum(x, y) {
-//     return x + y
-// }
-// alert(`Результат сложения чисел ${a} и ${b} равен  ${sum(a, b)}`);
-// // Задача 3: Написать функцию, которая принимает 
-// имя пользователя при ее вызове и выводит сообщение
-//  с приветствием пользователя по имени в консоль. 
-// Проверить работоспособность функции.
-// const username = prompt('Введите имя')
-
-
-// function greeting(name){
-//     console.log(`Привет, ${name}`)
-// }
-// greeting(username);
